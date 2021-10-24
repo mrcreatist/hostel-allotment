7e343317e0db888d07ea477965da03dbfc787bf2
Abhishek Verma <ahk.verma102@gmail.com>
2021-10-24 10:51:38 +0530

    declaring components

---
+++ src/app/component/component.ts
@@ -0,0 +1,9 @@
+import { HeaderComponent } from ".";
+import { AboutComponent } from "./about";
+import { MainComponent } from "./main";
+
+export const component = [
+    AboutComponent,
+    MainComponent,
+    HeaderComponent
+]
---
+++ src/app/component/index.ts
@@ -0,0 +1,5 @@
+export * from './about';
+export * from './main';
+export * from './header';
+
+export * from './component';
